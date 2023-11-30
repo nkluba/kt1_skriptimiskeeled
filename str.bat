07c02cbcf9dcda75144b12b8d0077674c07ad789
nkluba <146091133+nkluba@users.noreply.github.com>
2023-11-30 09:47:07 +0000

    characters and entities storing updated

--- str.bat
+++ str.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo off 
 chcp 65001 > nul
 setlocal enabledelayedexpansion
 
@@ -7,24 +7,26 @@ set str=%~1
 call :strLen str strlen
 echo String is %strlen% characters long
 
-set characters=ö ä ü õ š ž Ö Ä Ü Õ Š Ž
-set entities=ouml auml uuml otilde scaron zcaron Ouml Auml Uuml Otilde Scaron Zcaron
-
-for /L %%i in (0,1,%strlen%) do (
-    set "char=!str:~%%i,1!"
-    set "found="
-	echo !char!
-	for %%a in (%characters%) do ( 
-	    echo %%a 
-		if !char! equ %%a (
-            echo Found: !char!
-        )
-	    )
-	)
+rem Define an array for characters and their corresponding entities
+set /a idx=0
+for %%c in (ö ä ü õ š ž Ö Ä Ü Õ Š Ž) do (
+    set "convert[!idx!].character=%%c"
+    set /a idx+=1
 )
-	
+set /a idx=0
+for %%e in (ouml auml uuml otilde scaron zcaron Ouml Auml Uuml Otilde Scaron Zcaron) do (
+    set "convert[!idx!].entity=%%e"
+    set /a idx+=1
+)
+
+FOR /L %%i IN (0 1 11) DO  (
+   call echo %%convert[%%i].character%%
+   call echo %%convert[%%i].entity%%
+)
+
 pause
 exit /b
+
 
 :strLen
 if not "!%1:~%len%!"=="" set /A len+=1 & goto :strLen
