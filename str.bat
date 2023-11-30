caf3621ccaabfeba9235d89452ee5363447aca3d
nkluba <146091133+nkluba@users.noreply.github.com>
2023-11-30 07:38:43 +0000

    Update str.bat
    Check loop performance for numbers count (for test)

--- str.bat
+++ str.bat
@@ -5,10 +5,22 @@ set str=%~1
 
 call :strLen str strlen
 echo String is %strlen% characters long
+
+set "numericCount=0"
+
+for /L %%i in (0,1,%strlen%) do (
+    set "char=!str:~%%i,1!"
+    if !char! geq 0 if !char! leq 9 (
+        set /A numericCount+=1
+    )
+)
+
+echo Number of numeric characters in the string: %numericCount%
+
 pause
 exit /b
 
 :strLen
-   if not "!%1:~%len%!"=="" set /A len+=1 & goto :strLen
+if not "!%1:~%len%!"=="" set /A len+=1 & goto :strLen
 (endlocal & set %2=%len%)
 goto :eof
